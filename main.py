b7de3b5a16bfa0b8e5f64f2de7f958ab671ee3e8
Cyclonez <63996530+RedrcknRbn@users.noreply.github.com>
2026-05-22 23:46:31 -0500

    add in screenshare bc im silly :3

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import ctypes
 import configparser
 import time
 import textwrap
+from PIL import Image, ImageDraw, ImageGrab
 
 # Config-related thingies
 config = configparser.ConfigParser()
 config.read("config.ini")
-if "SETTINGS" in config: # there's *probably* a better way of doing this, but idk how to do it yet
+if "SETTINGS" in config:  # there's *probably* a better way of doing this, but idk how to do it yet
     if not "DLLPath" in config["SETTINGS"]:
         config["SETTINGS"]["DLLPath"] = r"C:\Program Files\Logitech Gaming Software\SDK\LCD\x64\LogitechLcd.dll"
     if not "AppletName" in config["SETTINGS"]:
         config["SETTINGS"]["AppletName"] = r"Python LCD Controller"
     if not "LCDType" in config["SETTINGS"]:
-        config["SETTINGS"]["LCDType"] = r"MONO" # we default to mono as it'll be the safest (i think)
+        # we default to mono as it'll be the safest (i think)
+        config["SETTINGS"]["LCDType"] = r"MONO"
 else:
     config["SETTINGS"] = {
         "DLLPath": r"C:\Program Files\Logitech Gaming Software\SDK\LCD\x64\LogitechLcd.dll",
@@ -24,19 +26,20 @@ else:
 
 with open('config.ini', 'w') as configfile:
     config.write(configfile)
-    
-## Init Config Vars
+
+# Init Config Vars
 DLLPath = config["SETTINGS"]["DLLPath"]
-AppletName = ctypes.c_wchar_p(config["SETTINGS"]["AppletName"]) # convert the applet name to the correct format for thhe dll
+# convert the applet name to the correct format for thhe dll
+AppletName = ctypes.c_wchar_p(config["SETTINGS"]["AppletName"])
 if config["SETTINGS"]["LCDType"] == "MONO":
-    LCDType = 0x00000001 # Mono is HEX 1
+    LCDType = 0x00000001  # Mono is HEX 1
 else:
-    LCDType = 0x00000002 # Color is HEX 2
+    LCDType = 0x00000002  # Color is HEX 2
 
 
 # Code
 
-## DLL Loading
+# DLL Loading
 try:
     LogiLCD = ctypes.CDLL(DLLPath)
     # attempt to load the needed DLL functions
@@ -48,26 +51,46 @@ try:
     MonoSetText = LogiLCD.LogiLcdMonoSetText
     MonoSetBackground = LogiLCD.LogiLcdMonoSetBackground
 except OSError as e:
-    print(f"Could not load DLL. Ensure Python is 64-bit and path is correct:\n{e}")
+    print(
+        f"Could not load DLL. Ensure Python is 64-bit and path is correct:\n{e}")
     exit(1)
 
-## Applet-related thingies
-init_success = LogiLCDInit(AppletName, LCDType) # create the applet
+# Applet-related thingies
+init_success = LogiLCDInit(AppletName, LCDType)  # create the applet
 if not init_success:
     print("Failed to initialize LGS LCD SDK. Is LGS running?")
     exit(1)
 
+# Functions to handle rendering
+
+
+def wrapText(inputText):
+    # Each line supports ~26-30 characters, so we'll need wrapping for each 26 characters (to be safe!)  -- SUBNOTE: IT TURNS OUT CHARACTERS ARENT MONOSPACED ON THIS THING. FUCCCCKKKKKKKKKKKKKKKKKKKK
+    # This also means we need to split it up into a max of 4 lines for the mono display
+    userText = textwrap.wrap(inputText, 26)[:4]
+    for i, text in enumerate(userText):
+        MonoSetText(i, ctypes.c_wchar_p(text))
+
+
+def sendImage(img):  # takes in a 160x43 Pillow image in monochrome mode
+    imgbytes = img.tobytes()
+    byte_array_type = ctypes.c_byte * 6880
+    lcd_buffer = byte_array_type.from_buffer(bytearray(imgbytes))
+    MonoSetBackground(lcd_buffer)
+
+
 try:
-    if LogiLCDConnection(LCDType): # check if an LCD is *actually* connected
-        # Each line supports ~26-30 characters, so we'll need wrapping for each 26 characters (to be safe!)  -- SUBNOTE: IT TURNS OUT CHARACTERS ARENT MONOSPACED ON THIS THING. FUCCCCKKKKKKKKKKKKKKKKKKKK
-        # This also means we need to split it up into a max of 4 lines for the mono display
-        userText = textwrap.wrap(input("Text: "),26)[:4]
-        for i, text in enumerate(userText):
-            MonoSetText(i, ctypes.c_wchar_p(text))
-        LogiLCDUpdate()
-        time.sleep(1)
-        input("Enter to shut down")
-        
+    if LogiLCDConnection(LCDType):  # check if an LCD is *actually* connected
+        while True:
+            img = ImageGrab.grab()
+            img = img.convert("L")
+            img = img.resize((160, 43), Image.Resampling.LANCZOS)
+            # mono devices are EVIL and only support 2 states (on/off)
+            img = img.point(lambda x: 255 if x >= 128 else 0)
+            sendImage(img)
+            LogiLCDUpdate()
+            time.sleep(1/30)
+
     else:
         print("SDK initalized, but no device was detected")
 
